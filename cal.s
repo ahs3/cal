364209f75b008293d0b624a80a757c4274481d11
Al Stone <ahs3@ahs3.net>
2020-08-20 00:21:13 -0600

    Lots of cleanup of stack usage, and now prints out a month
    Still need to truncate to the actual number of days in the month.
    
    Signed-off-by: Al Stone <ahs3@ahs3.net>

--- cal.s
+++ cal.s
@@ -23,7 +23,7 @@
 	.text
 #
 # --- data ---------------------------------------------------
-	.section	.rodata
+	.section .rodata
 	.align	3
 
 ARGS:		.string "month/year: %d/%d\n"
@@ -36,48 +36,90 @@ KVAL:		.string "k = %d\n"
 MVAL:		.string "m = %d\n"
 DVAL:		.string "d = %d\n"
 CVAL:		.string "c = %d\n"
-DBGDOW:		.string "[%d]%d "
-DBGEND:		.string "\n"
+DBG:		.string "[%d] %d\n"
 
-DOW0:		.string "sun"
-DOW1:		.string "mon"
-DOW2:		.string "tue"
-DOW3:		.string "wed"
-DOW4:		.string "thu"
-DOW5:		.string "fri"
-DOW6:		.string "sat"
+DOWS:		.string "sun","mon","tue","wed","thu","fri","sat"
+MONS:		.string "Jan","Feb","Mar","Apr","May","Jun"
+MONS2:		.string "Jul","Aug","Sep","Oct","Nov","Dec"
+
+MONHDR:		.string "      %3s %s\n"
+WKHDR:		.string "Su Mo Tu We Th Fr Sa\n"
+BUFOUT:		.string "%22s\n"
+BUFLEN:		.byte	22
+BLANK:		.byte	' '
+SPACES:		.string "   "
+NUMS:		.string "%2d "
+EOL:		.string "\n"
+
+	.bss
+	.align	4
+
+BUF:		.quad	0,0,0
 
 # --- functions -----------------------------------------------------
 	.text
 	.align	1
 #--------------------------------------------------------------------
 #
+#	initialize a buffer
+#
+	.global	bset
+	.type	bset, @function
+bset:
+	addi	sp,sp,-48		# function prologue
+	sd	ra,40(sp)
+	sd	s0,32(sp)
+	addi	s0,sp,48
+
+	mv	t1,a0			# address of buffer
+	mv	t2,a1			# length in bytes
+	mv	t3,a2			# value to use (bottom byte)
+	li	t4,0
+	mv	t5,a1
+loopi:
+	add	t1,t1,t4
+	bge	t4,t5,loopd
+	sb	t3,(t1)
+	addi	t4,t4,1
+	j	loopi
+	
+loopd:
+	mv	a0,t4
+	ld	ra,40(sp)		# function epilogue
+	ld	s0,32(sp)
+	addi	sp,sp,48
+	jr	ra
+
+#--------------------------------------------------------------------
+#
 #	convert a string
 #
 	.global	atoi
 	.type	atoi, @function
-
+atoi:
 	addi	sp,sp,-48		# function prologue
 	sd	ra,40(sp)
 	sd	s0,32(sp)
+	addi	s0,sp,48
 
+	sd	a0,-24(s0)		# save the string address
 	li	s2,0x30			# char '0'
 	li	s3,0			# char count
 	li	s4,10			# base 10
 	li	s5,0			# result
+	li	s6,9			# digit 9
 
-	mv	a5,a0
+	ld	s1,-24(s0)
 ATOI_LOOP:
-	ld	a7,0(a5)
-	beq	a7,x0,ATOI_SET		# at '\0', end of string
-	sub	a7,a7,s2
-	blt	a7,x0,ATOI_EXIT		# less than '0'?
-	li	a6,9
-	bgt	a7,a6,ATOI_EXIT		# greater than 9?
+	lb	s7,0(s1)
+	beq	s7,x0,ATOI_SET		# at '\0', end of string
+	sub	s7,s7,s2
+	blt	s7,x0,ATOI_EXIT		# less than '0'?
+	bgt	s7,s6,ATOI_EXIT		# greater than 9?
 	mul	s5,s5,s4		# we have another digit, mul x 10
-	add	s5,s5,a7		# add in the new digit
+	add	s5,s5,s7		# add in the new digit
 	addi	s3,s3,1			# count the digit
-	addi	a5,a5,8			# next char
+	addi	s1,s1,1			# next char
 	j	ATOI_LOOP
 
 ATOI_SET:
@@ -89,6 +131,7 @@ ATOI_EXIT:
 	ld	s0,32(sp)
 	addi	sp,sp,48
 	jr	ra
+
 #--------------------------------------------------------------------
 #
 #	find day of the week via Zeller's Rule
@@ -96,55 +139,53 @@ ATOI_EXIT:
 	.global	dow
 	.type	dow, @function
 dow:
-	addi	sp,sp,-48		# function prologue
-	sd	ra,40(sp)
-	sd	s0,32(sp)
+	addi	sp,sp,-80		# function prologue
+	sd	ra,72(sp)
+	sd	s0,64(sp)
+	addi	s0,sp,80
 #
-	sd	a0,-32(s0)		# save the month
-	sd	a1,-40(s0)		# save the day (k)
-	sd	a2,-48(s0)		# save the year
+	sd	a2,-64(s0)		# save the year
+	sd	a1,-56(s0)		# save the day (k)
+	sd	a0,-48(s0)		# save the month
 
-	ld	a1,-40(s0)		# debug: print k
+	ld	a1,-56(s0)		# debug: print k
 	lla	a0,KVAL
 	call	printf@plt
 
-	ld	s2,-32(s0)		# get the month
-	li	s3,2
-	sub	s2,s2,s3		# March is the first month, this time
-	bgt	s2,x0,doyear
-	addi	s2,s2,12		# so Jan,Feb are 11,12
+	ld	a3,-48(s0)		# get the month
+	addi	a3,a3,-2		# March is the first month, this time
+	bgt	a3,x0,doyear
+	addi	a3,a3,12		# so Jan,Feb are 11,12
 
 #
 doyear:	
-	sd	s2,-32(s0)		# save it for now (m)
+	sd	a3,-40(s0)		# save it for now (adjusted m)
 
-	mv	a1,s2			# debug: print m
+	mv	a1,a3			# debug: print m
 	lla	a0,MVAL
 	call	printf@plt
 
-	ld	s3,-48(s0)		# get the year
-	li	s4,100
-	rem	s3,s3,s4		# mod 100
-	ld	s2,-32(s0)		# get the month
-	li	s4,2
-	sub	s2,s2,s4		# March is the first month, this time
-	bgt	s2,x0,docent
-	li	s4,1
-	sub	s3,s3,s4
+	ld	a3,-64(s0)		# get the year
+	li	a4,0x64
+	rem	a3,a3,a4		# mod 100
+	ld	a4,-48(s0)		# get the month
+	addi	a5,a5,-2		# March is the first month, this time
+	bgt	a5,x0,docent
+	addi	a3,a3,-1
 
 docent:
-	sd	s3,-24(s0)		# save it for now (d)
+	sd	a3,-32(s0)		# save it for now (d)
 
-	ld	a1,-24(s0)		# debug: print d
+	ld	a1,-32(s0)		# debug: print d
 	lla	a0,DVAL
 	call	printf@plt
 
-	ld	s4,-48(s0)		# get the year
-	li	s5,100
-	div	s4,s4,s5		# y / 100 (yes, div)
-	sd	s4,-16(s0)		# save it for now (c)
+	ld	a3,-64(s0)		# get the year
+	li	a4,100
+	div	a3,a3,a4		# y / 100 (yes, div)
+	sd	a3,-24(s0)		# save it for now (c)
 
-	ld	a1,-16(s0)		# debug: print c
+	ld	a1,-24(s0)		# debug: print c
 	lla	a0,CVAL
 	call	printf@plt
 
@@ -152,43 +193,43 @@ docent:
 #	bring it all together
 #	dow = k + ((13*m-1) / 5) + d + (d / 4) + (c / 4) - (2 * c)
 #
-	ld	s7,-40(s0)		# retrieve the day = k
-	ld	s5,-32(s0)		# retrieve the adjusted month
-	li	s6,0xd
-	mul	s5,s5,s6		# m*13
-	li	s6,1
-	neg	s6,s6
-	add	s5,s5,s6		# (13*m-1)
-	li	s6,5
-	div	s5,s5,s6		# ((13*m-1) / 5)
-	add	s7,s7,s5		# k + ((13*m-1) / 5)
-	ld	s5,-24(s0)
-	add	s7,s7,s5		# k + ((13*m-1) / 5) + d
-	ld	s5,-24(s0)
-	srli	s5,s5,2
-	add	s7,s7,s5		# k + ((13*m-1)/5) + d + (d/4)
-	ld	s5,-16(s0)		# c
-	srli	s5,s5,2
-	add	s7,s7,s5		# k + ((13*m-1)/5) + d + (d/4) + (c/4)
-	ld	s5,-16(s0)		# c
-	slli	s5,s5,1
-	neg	s5,s5			# -(2*c)
-	add	s7,s7,s5		# k+((13*m-1)/5)+d+(d/4)+(c/4)-(2*c)
-	blt	s7,x0,dow7a
+	ld	a7,-56(s0)		# retrieve the day = k
+	ld	a5,-40(s0)		# retrieve the adjusted month
+	li	a6,0xd
+	mul	a5,a5,a6		# m*13
+	addi	a5,a5,-1		# (13*m-1)
+	li	a6,5
+	div	a5,a5,a6		# ((13*m-1) / 5)
+	add	a7,a7,a5		# k + ((13*m-1) / 5)
+	ld	a5,-32(s0)
+	add	a7,a7,a5		# k + ((13*m-1) / 5) + d
+	ld	a5,-32(s0)
+	srli	a5,a5,2
+	add	a7,a7,a5		# k + ((13*m-1)/5) + d + (d/4)
+	ld	a5,-24(s0)		# c
+	srli	a5,a5,2
+	add	a7,a7,a5		# k + ((13*m-1)/5) + d + (d/4) + (c/4)
+	ld	a5,-24(s0)		# c
+	slli	a5,a5,1
+	neg	a5,a5			# -(2*c)
+	add	a7,a7,a5		# k+((13*m-1)/5)+d+(d/4)+(c/4)-(2*c)
+	blt	a7,x0,dow7a
 
-	li	s6,7
-	rem	s7,s7,s6
+	li	a6,7
+	rem	a7,a7,a6
 	j	dwdone
 
-dow7a:	li	s6,-7
-	rem	s7,s7,s7
-	addi	s7,s7,7
+dow7a:	li	a6,-7
+	rem	a7,a7,a6
+	addi	a7,a7,7
 #
-dwdone:	mv	a0,s7
-	ld	ra,40(sp)		# function epilogue
-	ld	s0,32(sp)
-	addi	sp,sp,48
+dwdone:
+	mv	a0,a7
+	ld	ra,72(sp)		# function epilogue
+	ld	s0,64(sp)
+	addi	sp,sp,80
 	jr	ra
+
 #--------------------------------------------------------------------
 #
 #	main program
@@ -196,62 +237,126 @@ dwdone:	mv	a0,s7
 	.global	main
 	.type	main, @function
 main:
-	addi	sp,sp,-48		# function prologue
-	sd	ra,40(sp)
-	sd	s0,32(sp)
-
+	addi	sp,sp,-80		# function prologue
+	sd	ra,72(sp)
+	sd	s0,64(sp)
+	addi	s0,sp,80
 #
 #	right number of args?
 #
+
 	li	t0,3
 	blt	a0,t0,ERR_NARGS
 #
-	addi	s0,sp,48
-	sd	a0,-40(s0)		# save argc
+	sd	a0,-56(s0)		# save argc
 	sd	a1,-48(s0)		# save &argv
 #
 # 	first arg is the month number
 #
-	ld	a5,-48(s0)		# load &argv
-	ld	a5,8(a5)		# load &argv[1]
-	mv	a0,a5
+	ld	t5,-48(s0)		# load &argv
+	ld	t5,8(t5)		# load &argv[1]
+	mv	a0,t5
 	call	atoi
 	blt	a0,x0,ERR_NINT_MONTH
-	sd	a0,-32(s0)
+	sd	a0,-40(s0)		# month number
 #
 #	second arg is the year number
 #
-	ld	a5,-48(s0)		# load &argv
-	ld	a5,16(a5)		# load &argv[2]
-	mv	a0,a5
+	ld	t5,-48(s0)		# load &argv
+	ld	t5,16(t5)		# load &argv[2]
+	mv	a0,t5
 	call	atoi
 	blt	a0,x0,ERR_NINT_YEAR
-	sd	a0,-24(s0)
+	sd	a0,-32(s0)		# year number
 #
 #	now we know what we need to look up
 #
-	ld	a2,-24(s0)
-	ld	a1,-32(s0)
+	ld	a2,-32(s0)
+	ld	a1,-40(s0)
 	lla	a0,ARGS
 	call	printf@plt
 #
 #	figure out which day of the week the 1st is
 #
-	ld	a0,-32(s0)		# month is the first arg
+	ld	a2,-32(s0)		# year is the third arg
 	li	a1,1			# always the first of the month
-	ld	a2,-24(s0)		# year is the third arg
+	ld	a0,-40(s0)		# month is the first arg
 	call	dow
-	sd	a0,-16(s0)		# save the day of week, sun == 0
+	sd	a0,-24(s0)		# save the day of week, sun == 0
 
-	ld	a2,-16(s0)
-	ld	a6,-16(s0)		# get DOW string address for a1
+	ld	a2,-24(s0)
+	ld	a6,-24(s0)		# get DOW string address for a1
 	slli	a6,a6,2
-	lla	a7,DOW0
+	lla	a7,DOWS
 	add	a1,a7,a6
 	lla	a0,DOW
 	call	printf@plt		# print some debug info
 
+	la	a2,BLANK
+	lb	a2,0(a2)
+	la	a1,BUFLEN
+	lb	a1,0(a1)
 	lla	a0,BUF
+	call	bset
+
+	ld	t0,-48(s0)		# load &argv
+	ld	t0,16(t0)		# load &argv[2]
+	mv	a2,t0			# ptr to year string
+	lla	t0,MONS
+	ld	t1,-40(s0)
+	addi	t1,t1,-1
+	slli	t1,t1,2
+	add	a1,t0,t1		# month string
+	lla	a0,MONHDR
+	call	printf@plt
+
+	lla	a0,WKHDR
+	call	printf@plt
+	
+	ld	t0,-24(s0)
+mloop0:
+	ble	t0,x0,mloop1
+	lla	a0,SPACES
+	call	printf@plt
+	addi	t0,t0,-1
+	j	mloop0
+
+mloop1:
+	li	s2,1
+	mv	a1,s2
+	lla	a0,NUMS
+	call	printf@plt		
+
+	li	a4,1			# day-of-month counter
+	sd	a4,-32(s0)
+	li	a5,7
+	li	a6,0x1f
+
+mloop2:
+	ld	a4,-32(s0)		# day-of-month counter
+	addi	a4,a4,1
+	sd	a4,-32(s0)
+	li	a6,0x1f
+	bgt	a4,a6,mloop5
+	ld	a3,-24(s0)		# day-of-week counter
+	addi	a3,a3,1
+	sd	a3,-24(s0)
+	li	a5,7
+	rem	a3,a3,a5
+	beq	a3,x0,mloop4
+mloop3:
+	ld	a1,-32(s0)
+	lla	a0,NUMS
+	call	printf@plt		
+	j	mloop2
+
+mloop4:
+	lla	a0,EOL
+	call	printf@plt		
+	j	mloop3
+
+mloop5:
+	lla	a0,EOL
 	call	printf@plt		
 
 	j	EXIT			# all done
@@ -291,9 +396,9 @@ ERR_NARGS:
 #
 EXIT:
 	li	a0,42			# function return value
-	ld	ra,40(sp)		# function epilogue
-	ld	s0,32(sp)
-	addi	sp,sp,48
+	ld	ra,72(sp)		# function epilogue
+	ld	s0,64(sp)
+	addi	sp,sp,80
 	jr	ra
 #
 #	program end
